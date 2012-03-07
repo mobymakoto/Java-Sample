abc3e22c289930285e2f4ad423e1f81fddb18f3b
makoto nakajima <mobygo1024@gmail.com>
2012-03-07 16:25:40 +0900

    modified:   testsuite/com/moby/jms/sample/JmsConnectorTest.java 	modified:   .classpath 	modified:   src/com/moby/jms/sample/JmsConnector.java 	modified:   src/com/moby/jms/sample/JmsReceiver.java 	deleted:    src/com/moby/jms/sample/testsuite/JmsReceiverTest.java 	deleted:    src/com/moby/jms/sample/testsuite/JmsSenderTest.java

--- testsuite/com/moby/jms/sample/JmsConnectorTest.java
+++ testsuite/com/moby/jms/sample/JmsConnectorTest.java
@@ -2,6 +2,8 @@ package com.moby.jms.sample;
 
 import static org.junit.Assert.*;
 
+import javax.jms.JMSException;
+
 import org.junit.Test;
 
 import com.moby.jms.sample.JmsConnector;
@@ -19,17 +21,29 @@ public class JmsConnectorTest {
 
     @Test
     public void testGetSession() {
-        fail("まだ実装されていません");
+        try {
+            assertNotNull(JmsConnector.build("queue").getSession());
+        } catch (JMSConnectorException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void testGetMessageProducer() {
-        fail("まだ実装されていません");
+        try {
+            assertNotNull(JmsConnector.build("topic").getMessageProducer());
+        } catch (JMSException | JMSConnectorException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void testGetMessageConsumger() {
-        fail("まだ実装されていません");
+    public void testGetMessageConsumer() {
+        try {
+            assertNotNull(JmsConnector.build("topic").getMessageConsumer());
+        } catch (JMSException | JMSConnectorException e) {
+            e.printStackTrace();
+        }
     }
 
 }
